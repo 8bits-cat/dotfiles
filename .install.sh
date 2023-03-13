ddf2244ffa2f6bc2a32927ed2e3e96b88a2b277e
8bits-cat <X103h92@Gmail.com>
2023-03-13 23:24:13 +0800

    first version

---
+++ .install.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+############################
+# ./install.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles,also some powerful tools for myself
+############################
+
+########## Variables
+
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files=$(ls "$dir")    # list of files/folders to symlink in homedir
+
+##########
+
+# create dotfiles_old in homedir
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+mkdir -p $olddir
+echo "done"
+
+# change to the dotfiles directory
+echo -n "Changing to the $dir directory ..."
+cd $dir
+echo "done"
+
+################################
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+################################
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -sf $dir/$file ~/.$file
+done
+
+###################################
+#     install powerful tools
+###################################
+# install zsh and zplug
+install_zsh_zplug () {
+# Test to see if zshell is installed.  If it is:
+if [[ -f /bin/zsh || -f /usr/bin/zsh ]]; then
+    if [[ ! -d ~/.zplug ]]; then
+	    sudo apt install zplug
+    fi
+    # Set the default shell to zsh if it isn't currently set to zsh
+    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+        chsh -s $(which zsh)
+    fi
+else
+    # If zsh isn't installed, get the platform of the current machine
+    platform=$(uname);
+    # If the platform is Linux, try an apt-get to install zsh and then recurse
+    if [[ $platform == 'Linux' ]]; then
+        if [[ -f /etc/redhat-release ]]; then
+            sudo yum install zsh
+            install_zsh_zplug
+        fi
+        if [[ -f /etc/debian_version ]]; then
+            sudo apt install zsh
+            install_zsh_zplug
+        fi
+    # If the platform is OS X, tell the user to install zsh :)
+    elif [[ $platform == 'Darwin' ]]; then
+        echo "Please install zsh, then re-run this script!"
+        exit
+    fi
+fi
+}
+
+install_zsh_zplug
+
+# install tmux
+install_tmux () {
+# Test to see if tmuxis installed:
+if [[ ! -f /bin/tmux || ! -f /usr/bin/tmux ]]; then
+    platform=$(uname);
+    # If the platform is Linux, try an apt-get to install tmux and then recurse
+    if [[ $platform == 'Linux' ]]; then
+        if [[ -f /etc/redhat-release ]]; then
+            sudo yum install tmux 
+        fi
+        if [[ -f /etc/debian_version ]]; then
+            sudo apt install tmux 
+        fi
+    # If the platform is OS X, tell the user to install tmux :)
+    elif [[ $platform == 'Darwin' ]]; then
+        echo "Please install tmux, then re-run this script!"
+        exit
+    fi
+fi 
+}
+
+install_tmux
+
+# install fzf
+install_fzf (){
+    if [[ ! -d ~/.fzf ]]; then
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+    fi
+}
+
+install_fzf
---
+++ zshrc
@@ -0,0 +1,47 @@
+# set path if required
+# export PATH="$PATH:/usr/bin"
+
+# Aliases
+if [ -f ~/.aliases ]; then
+	. ~/.aliases
+fi
+
+# zplug - manage plugins
+source /usr/share/zplug/init.zsh
+zplug "plugins/git", from:oh-my-zsh
+zplug "plugins/sudo", from:oh-my-zsh
+zplug "plugins/command-not-found", from:oh-my-zsh
+zplug "zsh-users/zsh-syntax-highlighting"
+zplug "zsh-users/zsh-autosuggestions"
+zplug "zsh-users/zsh-history-substring-search"
+zplug "zsh-users/zsh-completions"
+zplug "themes/robbyrussell", from:oh-my-zsh, as:theme   # Theme
+zplug "jeffreytse/zsh-vi-mode"	# A better and friendly vi(vim) mode plugin for ZSH
+
+# zplug - install/load new plugins when zsh is started or reloaded
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo;zplug install
+    fi
+fi
+zplug load --verbose
+
+# Set up the prompt - if you load Theme with zplugin as in this example, this will be overriden by the Theme. If you comment out the Theme in zplugins, this will be loaded.
+autoload -Uz promptinit
+promptinit
+prompt walters # see Zsh Prompt Theme below
+
+# Keep 5000 lines of history within the shell and save it to ~/.zsh_history:
+HISTSIZE=5000
+SAVEHIST=5000
+HISTFILE=~/.zsh_history
+
+# Use modern completion system
+autoload -Uz compinit
+compinit
+## 'Tab' to auto switch command case
+zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*'
+
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
